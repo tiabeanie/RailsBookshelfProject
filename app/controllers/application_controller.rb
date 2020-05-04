551ed75459109cce451b9cecf1ec2be02d7efe00
Sydney <sydney.jane.hunter@gmail.com>
2020-05-04 17:24:33 -0600

    updated application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        @user = User.find_by(id: session[:user_id])
+    end
+
+    def logged_in? 
+        !!current_user
+    end
+
+    def require_login
+        unless logged_in?
+            flash[:danger] = "You have to be logged in for that"
+            redirect_to login_path
+        end
+    end
 end
+
