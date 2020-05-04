cf904f162bbdbe18bd94c7d14ab14c7af959c557
Sydney <sydney.jane.hunter@gmail.com>
2020-05-04 17:26:38 -0600

    updated authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,12 @@
 class AuthorsController < ApplicationController
+    before_action :require_login
+
+    def index
+        @authors = Author.all 
+    end
+
+    def book 
+        @author = Author.find_by(id: params[:id])
+        @books = @author.books
+    end
 end
