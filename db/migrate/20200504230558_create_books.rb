ddcc62b34389e024785dd66712e3ccb48529246f
Sydney <sydney.jane.hunter@gmail.com>
2020-05-04 17:06:37 -0600

    created book resource

---
+++ db/migrate/20200504230558_create_books.rb
@@ -0,0 +1,13 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.text :description
+      t.integer :author_id
+      t.iteger :current_chapter
+      t.integer :created_by
+
+      t.timestamps
+    end
+  end
+end
