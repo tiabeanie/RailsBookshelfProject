28b745894c15cfabbe6f1db8499b9865dc8ecf00
Sydney <sydney.jane.hunter@gmail.com>
2020-05-04 18:02:13 -0600

    updated entries controller

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,2 +1,53 @@
 class EntriesController < ApplicationController
+    before_action :require_login
+    before_action :set_entry, only: [:edit, :update, :destroy]
+
+    def new
+        @entry = Entry.new
+        @entry.book = Book.find_by(id: params[:book_id])
+    end
+
+    def create
+        @entry = Entry.new(entry_params)
+        @entry.book = Book.find_by(id: params[:book_id])
+        if @entry.save
+            flash[:message] = "#{@entry.book.title} has been added to your bookshelf"
+            redirect_to book_path(@entry.book)
+        else
+            render :new
+        end
+    end
+
+    def edit
+    end
+
+    def update
+        @entry.update(entry_params)
+        if @entry.save
+            flash[:message] = "#{@entry.book.title} entry has been updated"
+            redirect_to book_path(@entry.book)
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        if @entry
+            @entry.destroy
+            flash[:message] = "The book has been removed from your bookshelf"
+            redirect_to bookshelves_path
+        else
+            flash[:danger] = "The book could not be removed"
+            render 'books/show'
+        end
+    end
+
+    private 
+    def entry_params
+        params.require(:entry).permit(:bookshelf_id, :user_id, :user_notes, :user_chapter)
+    end
+
+    def set_entry
+        @entry = Entry.find_by(id: params[:id])
+    end
 end
