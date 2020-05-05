580a01909a499668d689109c7500c4914faa820a
Sydney <sydney.jane.hunter@gmail.com>
2020-05-04 18:04:03 -0600

    updated genre controller

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,12 @@
 class GenresController < ApplicationController
+    before_action :require_login
+
+    def index
+        @genres = Genre.all 
+    end
+
+    def show
+        @genre = Genre.find_by(id: params[:id])
+        @books = @genre.books
+    end
 end
